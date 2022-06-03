8744b39fb752d38d0297b7d2e5e275cb5de583ff
nickick <hong.nick@gmail.com>
2022-06-03 17:40:53 -0400

    Add footer

--- src/Footer.js
+++ src/Footer.js
@@ -1,5 +1,8 @@
-import { Container, keyframes } from '@mui/material';
+import {
+  Box, Container, keyframes, Typography,
+} from '@mui/material';
 import { useContext } from 'react';
+import NextLink from 'next/link';
 import { entranceAnimationDuration } from './constants';
 import { LoadedContext } from './LoadedContextProvider';
 import ThemeToggle from './ThemeToggle';
@@ -24,11 +27,129 @@ export default function Footer() {
     <Container
       sx={{
         maxWidth: '1440px',
-        px: 4,
-        animation: `${fadeFromBelow} ${entranceAnimationDuration}s both ${animationDelay}s`,
+        p: 4,
+        animation: `${fadeFromBelow} ${entranceAnimationDuration}s both ${1 + animationDelay}s`,
         zIndex: 3,
       }}
     >
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+          width: '100%',
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 2,
+          }}
+        >
+          <Typography
+            variant="h3"
+          >
+            To the moon and never back.
+          </Typography>
+          <Typography
+            variant="quote"
+          >
+            Email
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="mailto: driftershoots@gmail.com"
+            >
+              driftershoots@gmail.com
+            </NextLink>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+          }}
+        >
+          <Typography
+            variant="quote"
+          >
+            Sitemap
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="/"
+            >
+              Home
+            </NextLink>
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="/publications"
+            >
+              Publications
+            </NextLink>
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="/gallery"
+            >
+              Gallery
+            </NextLink>
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="/about"
+            >
+              About
+            </NextLink>
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="/contact"
+            >
+              Contact
+            </NextLink>
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            flex: 1,
+          }}
+        >
+          <Typography
+            variant="h4"
+          >
+            Information
+          </Typography>
+          <Typography
+            variant="body"
+          >
+            <NextLink
+              href="privacy-policy"
+            >
+              Privacy Policy
+            </NextLink>
+          </Typography>
+        </Box>
+      </Box>
       <ThemeToggle />
     </Container>
   );
