fa074048b41e75247ca292e6228ad9c7d1416f7f
Nick Hong <me@nickhong.xyz>
2025-05-11 10:30:50 +0800

    Get basics of book page

--- src/PhotoBook.tsx
+++ src/PhotoBook.tsx
@@ -1,25 +1,68 @@
-import { Box } from '@mui/material';
+import { Box, Typography } from '@mui/material';
 import { OrderButton } from './OrderButton';
 
 const PhotoBook = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-4xl font-bold">Book</h1>
+    <Box
+      sx={{
+        zIndex: 3,
+        px: {
+          xs: 3,
+          md: 14,
+        },
+        mt: {
+          xs: 4,
+          md: 10,
+        },
+        width: '100%',
+        maxWidth: '1600px',
+        margin: 'auto',
+      }}
+    >
       <Box
         sx={{
           display: 'flex',
           flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          zIndex: 1000,
-          position: 'relative',
+          width: '100%',
         }}
       >
-        <OrderButton>
-          <span>Order</span>
-        </OrderButton>
+        <Typography variant="h1">It Was Never Dark</Typography>
+
+        <Box>
+          <Typography
+            variant="body1"
+            sx={{
+              fontSize: '2rem',
+              lineHeight: '2rem',
+              letterSpacing: '0.1rem',
+            }}
+          >
+            &quot;It Was Never Dark&quot; is the culmination of seven years of
+            Drift&apos;s photographic career spanning five different continents.
+            The book contains written excerpts and multimedia evidence from his
+            incarceration and details his journey from there to contemporary
+            artist.
+          </Typography>
+        </Box>
+        <Box
+          sx={{
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: {
+              xs: 'center',
+              md: 'flex-end',
+            },
+            justifyContent: 'center',
+            zIndex: 1000,
+            position: 'relative',
+          }}
+        >
+          <OrderButton>
+            <Typography variant="h4">Preorder</Typography>
+          </OrderButton>
+        </Box>
       </Box>
-    </div>
+    </Box>
   );
 };
 
