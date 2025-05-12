af8543ee93c464967ef256405429817d391a6f78
Nick Hong <me@nickhong.xyz>
2025-05-13 00:05:05 +0800

    add more preorder book stuff

--- src/PhotoBook.tsx
+++ src/PhotoBook.tsx
@@ -1,5 +1,8 @@
 import { Box, Typography } from '@mui/material';
+import Image from 'next/image';
+import { fadeIn } from './GalleryV2/animations';
 import { OrderButton } from './OrderButton';
+import { entranceAnimationDelay, entranceAnimationDuration } from './constants';
 
 const PhotoBook = () => {
   return (
@@ -15,8 +18,9 @@ const PhotoBook = () => {
           md: 10,
         },
         width: '100%',
-        maxWidth: '1600px',
+        maxWidth: '1024px',
         margin: 'auto',
+        animation: `${fadeIn} ${entranceAnimationDuration}s both ${entranceAnimationDelay}s`,
       }}
     >
       <Box
@@ -27,7 +31,76 @@ const PhotoBook = () => {
         }}
       >
         <Typography variant="h1">It Was Never Dark</Typography>
-
+        <Box
+          sx={{
+            width: {
+              xs: '100%',
+              md: '100%',
+            },
+            my: {
+              xs: 4,
+              md: 4,
+            },
+            display: 'grid',
+            gridTemplateColumns: {
+              xs: '1fr 1fr',
+              md: '1fr 1fr 1fr 1fr',
+            },
+            alignItems: 'center',
+            gap: 4,
+          }}
+        >
+          <Image
+            src="/book/after-the-storm.jpg"
+            alt="It Was Never Dark"
+            width={1000}
+            height={1500}
+            style={{
+              width: '100%',
+              height: 'auto',
+              objectFit: 'cover',
+              borderRadius: '10px',
+            }}
+          />
+          <Image
+            src="/book/and-when-we-die-it-will-feel-like-this.jpg"
+            alt="And When We Die It Will Feel Like This"
+            width={1500}
+            height={1000}
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxHeight: '500px',
+              objectFit: 'cover',
+              borderRadius: '10px',
+            }}
+          />
+          <Image
+            src="/book/heartbeat.jpg"
+            alt="Heartbeat"
+            width={1000}
+            height={1500}
+            style={{
+              width: '100%',
+              height: 'auto',
+              objectFit: 'cover',
+              borderRadius: '10px',
+            }}
+          />
+          <Image
+            src="/book/foreshadowing-small.jpg"
+            alt="Foreshadowing"
+            width={1500}
+            height={1000}
+            style={{
+              width: '100%',
+              height: 'auto',
+              maxHeight: '500px',
+              objectFit: 'cover',
+              borderRadius: '10px',
+            }}
+          />
+        </Box>
         <Box>
           <Typography
             variant="body1"
@@ -35,6 +108,8 @@ const PhotoBook = () => {
               fontSize: '2rem',
               lineHeight: '2rem',
               letterSpacing: '0.1rem',
+              maxWidth: '800px',
+              mt: 3,
             }}
           >
             &quot;It Was Never Dark&quot; is the culmination of seven years of
@@ -49,8 +124,12 @@ const PhotoBook = () => {
             display: 'flex',
             flexDirection: 'column',
             alignItems: {
-              xs: 'center',
-              md: 'flex-end',
+              xs: 'flex-end',
+              md: 'flex-start',
+            },
+            mt: {
+              xs: 4,
+              md: 4,
             },
             justifyContent: 'center',
             zIndex: 1000,
